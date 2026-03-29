7c0247b742cb849d611df7507ee16b4e03da660a
cherrythecoder <cherrythecoder@gmail.com>
2026-03-29 16:47:29 -0400

    Initial Commit

---
+++ summary/summaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  summaryApp.swift
+//  summary
+//
+//  Created by charan on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct summaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ summaryTests/summaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  summaryTests.swift
+//  summaryTests
+//
+//  Created by charan on 3/29/26.
+//
+
+import Testing
+@testable import summary
+
+struct summaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
